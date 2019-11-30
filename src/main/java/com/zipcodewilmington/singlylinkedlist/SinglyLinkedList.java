200284df3955a71f2d7b5a2f2737dae815f6d5d0
Mike King <michkin32@gmail.com>
2019-11-29 20:38:06 -0500

    more test done

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,102 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+
+    public class Node {
+        Integer data;
+        Node next;
+
+
+        public Node(Integer data){
+            this.data = data;
+            this.next = null;
+
+        }
+
+    }
+    public Node head = null;
+    public Node tail = null;
+
+    public void addNode(int data){
+        Node newNode = new Node(data);
+        newNode.next = null;
+
+        if(this.head == null){
+            head = newNode;
+
+
+        }
+        else{
+            Node lastNode = this.head;
+            while (lastNode.next != null){
+                lastNode = lastNode.next;
+            }
+
+            lastNode.next = newNode;
+
+        }
+
+    }
+    public Boolean contains(Integer element){
+        Node lastNode = this.head;
+        while (lastNode.next != null){
+            lastNode = lastNode.next;
+            if(lastNode.data == element){
+                return true;
+            }
+        }
+        return false;
+    }
+    public int find(Integer element){
+        Node lastNode = this.head;
+        while (lastNode != null){
+
+            int counter = 0;
+            if(lastNode.data == element && this.head != lastNode){
+                counter++;
+                return counter;
+            }else if(lastNode.data == element){
+                return counter;
+            }
+            counter++;
+            lastNode = lastNode.next;
+        }
+        return -1;
+    }
+
+    public Integer size(){
+        Node lastNode = this.head;
+        int counter = 0;
+        while (lastNode != null){
+
+            counter++;
+            lastNode = lastNode.next;
+        }
+        return counter;
+    }
+    public Integer get( Integer index){
+        Node lastNode = this.head;
+        if(index <= this.size()){
+        for (int i = 0; i < index; i++) {
+           lastNode = lastNode.next;
+
+        }
+        }
+        if (lastNode.data != null && index <= this.size()){
+        return lastNode.data;
+        }
+        return -1;
+    }
+    public void remove(int index){
+        if (index == 0){
+             head = head.next;
+        }
+    }
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,68 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void addTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        Integer two = 2;
+
+        list.addNode(2);
+        list.addNode(3);
+        Assert.assertEquals((Integer)list.head.data, two);
+        Assert.assertEquals(list.head.next.data, (Integer)3);
+    }
+    @Test
+    public void containsTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        Integer two = 2;
+
+        list.addNode(2);
+        list.addNode(3);
+        Assert.assertEquals((Integer)list.head.data, two);
+        Assert.assertFalse(list.contains(5));
+        Assert.assertTrue(list.contains(3));
+    }
+    @Test
+    public void findTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+
+
+        list.addNode(2);
+        list.addNode(3);
+        Assert.assertEquals(list.find(3), 1);
+        Assert.assertEquals(list.find(2), 0);
+        Assert.assertEquals(list.find(4), -1);
+    }
+    @Test
+    public void sizeTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+
+
+        list.addNode(2);
+        list.addNode(3);
+        list.addNode(6);
+        Assert.assertEquals(list.size(), (Integer)3);
+
+
+}
+    @Test
+    public void getTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+
+
+        list.addNode(2);
+        list.addNode(3);
+        list.addNode(6);
+        Assert.assertEquals(list.get(2), (Integer)6);
+        Assert.assertEquals(list.get(4), (Integer)(-1));
+
+
+    }
 }
